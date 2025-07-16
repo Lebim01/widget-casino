03fe1dcc3f702574e44c58ce4b0b48172d631034
Victor Alvarez <victoralvarezsaucedo@gmail.com>
2025-07-16 16:28:13 -0700

    dvh

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { Toaster } from "react-hot-toast";
 
 export default function Home() {
   return (
-    <div className="relative w-full h-screen bg-white">
+    <div className="relative w-full h-[100dvh] bg-white">
       <Toaster />
       <Widget />
       <iframe id="dota" src="https://dota.click" className="h-full w-full" />
